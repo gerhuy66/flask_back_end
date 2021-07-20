aca6ef5c5e3600d71975b2e4cee6eb2062d9f490
huy le <huy.cdl@cyberlogitec.com>
2021-07-20 14:02:08 +0700

    increase time out

--- app/service/elastic/__init__.py
+++ app/service/elastic/__init__.py
@@ -4,13 +4,13 @@ from tika import parser
 import io
 
 service = 'es'
-access_key = ''
-serect_key = ''
+access_key = 'AKIAXYZRVWASFJT4DGP7'
+serect_key = '0aZ9MB6OIiELdf4JJQ61Z4cg8MXo3yBlPGKm+9RR'
 region = 'ap-southeast-1'
 awsauth = AWS4Auth(access_key, serect_key,region,service)
 import datetime
 
-es = Elasticsearch(["https://search-cvsearch-2hgyzv2t5gdogt27c3ehhxrxku.ap-southeast-1.es.amazonaws.com:443"],http_auth = awsauth,use_ssl = True,verify_certs = True,connection_class = RequestsHttpConnection,request_timeout = 30)
+es = Elasticsearch(["https://search-cvsearch-2hgyzv2t5gdogt27c3ehhxrxku.ap-southeast-1.es.amazonaws.com:443"],http_auth = awsauth,use_ssl = True,verify_certs = True,connection_class = RequestsHttpConnection,request_timeout = 30,timeout=30)
 import regex as re
 def convert_elastic(file):
     print(file)
